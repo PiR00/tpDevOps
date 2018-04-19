737a054c9aa9ef2d182fb1dba90a80afbf42cb61
azing76 <azing76@gmail.com>
2018-04-19 16:10:34 +0200

    dockerfile + compose

--- tpDevOps/Startup.cs
+++ tpDevOps/Startup.cs
@@ -11,4 +11,26 @@ namespace tpDevOps
             ConfigureAuth(app);
         }
     }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Database connection string.
+        // Make sure to update the Password value below from "Your_password123" to your actual password.
+        var connection = @"Server=db;Database=master;User=sa;Password=Your_password123;";
+
+        // This line uses 'UseSqlServer' in the 'options' parameter
+        // with the connection string defined above.
+        services.AddDbContext<ApplicationDbContext>(
+            options => options.UseSqlServer(connection));
+
+        services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        services.AddMvc();
+
+        // Add application services.
+        services.AddTransient<IEmailSender, AuthMessageSender>();
+        services.AddTransient<ISmsSender, AuthMessageSender>();
+    }
 }
